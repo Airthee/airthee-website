a3f7db216a024ea6549d5ba09c1c1fd18ff70cdf
Airthee <3355624+Airthee@users.noreply.github.com>
2020-01-11 21:51:49 +0100

    :lipstick: lint

--- pages/contact.vue
+++ pages/contact.vue
@@ -13,14 +13,14 @@
         </p>
 
         <div v-if="formGlobalNotification !== null" :class="formGlobalNotification.class" class="notification">
-          <button class="delete" @click="formGlobalNotification = null" />
+          <button @click="formGlobalNotification = null" class="delete" />
           {{ formGlobalNotification.message }}
         </div>
 
         <form
           ref="formContact"
-          method="post"
           @submit.prevent="submitForm"
+          method="post"
         >
           <div class="field">
             <label class="label">Name</label>
