991f8e1b4c2e267ec6b4c0518d72bfa6531c3e1a
Airthee <13355624+Airthee@users.noreply.github.com>
2020-01-13 15:56:30 +0100

    :art: Merge i18n data in one file

--- i18n/fr-FR.js
+++ i18n/fr-FR.js
@@ -1,5 +1,4 @@
 import globalTranslations from './global.data'
-import userTranslations from './fr-FR.data'
 
 export default {
   home: {
@@ -54,6 +53,77 @@ export default {
     creditsLabel: 'Crédits',
     githubText: 'Le code de ce projet est disponible sur '
   },
-  ...userTranslations,
+  mainPicture: {
+    alt: 'Photo de moi sur le pont de Brooklyn à New York'
+  },
+  informations: [
+    'Raphaël TISON (@Airthee)',
+    '23 ans',
+    'Vendée, France'
+  ],
+  timeline: [
+    {
+      title: 'EPSI (Nantes, 44200, France)',
+      description: 'RNCP 1 - Expert en informatique & système d’information',
+      from: new Date(2018, 9 - 1) // september
+    },
+    {
+      title: 'IUT (La Roche sur Yon, 85000, France)',
+      description: 'Licence Professionnelle DIP',
+      from: new Date(2017, 9 - 1) // september
+    },
+    {
+      title: 'Lycée Jean de Lattre De Tassigny (La Roche sur Yon, 85000, France)',
+      description: 'BTS Systèmes Numériques',
+      from: new Date(2015, 9 - 1) // september
+    },
+    {
+      title: 'Lycée Jean de Lattre De Tassigny (La Roche sur Yon, 85000, France)',
+      description: 'BAC STI2D',
+      from: new Date(2013, 9 - 1) // september
+    }
+  ],
+  socialNetworks: [
+    {
+      name: 'Github',
+      link: 'https://github.com/Airthee',
+      img: '/images/socials/github.png',
+      imgAlt: 'Logo Github'
+    },
+    {
+      name: 'Twitter',
+      link: 'https://twitter.com/Airthee',
+      img: '/images/socials/twitter.png',
+      imgAlt: 'Logo Twitter'
+    },
+    {
+      name: 'LinkedIn',
+      link: 'https://www.linkedin.com/in/raphael-tison',
+      img: '/images/socials/linkedin.png',
+      imgAlt: 'Logo LinkedIn'
+    }
+  ],
+  citations: [
+    {
+      text: 'Un objectif n’est pas toujours censé être atteint, il sert surtout de direction, de moteur, et d’accélérateur.',
+      author: 'Bruce Lee'
+    },
+    {
+      text: 'La seule façon de faire du bon travail est d’aimer ce que vous faites. Si vous n’avez pas encore trouvé, continuez à chercher.',
+      author: 'Steve Jobs'
+    },
+    {
+      text: 'Croire en soi est la première étape pour atteindre un objectif. Si vous pensez que vous allez échouer, vous le ferez probablement.',
+      author: 'Will Smith'
+    },
+    {
+      text: 'Si vous voulez mener une vie heureuse, associez-la à un objectif, et non à des personnes ou à des objets.',
+      author: 'Albert Einstein'
+    },
+    {
+      text: "L'échec est le fondement de la réussite.",
+      author: 'Lao Tseu'
+    }
+  ],
   ...globalTranslations
 }
