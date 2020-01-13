97ddd5488d5659ea22880f482b1f88d826327337
Airthee <13355624+Airthee@users.noreply.github.com>
2020-01-13 13:29:38 +0100

    :lipstick: Add hero on all pages

--- pages/credits.vue
+++ pages/credits.vue
@@ -1,26 +1,32 @@
 <template>
-  <section class="section has-background-white">
-    <div class="container">
-      <h1 v-t="'credits.title'" class="title" />
-      <div class="content">
-        <div class="page-description">
+  <div>
+    <section class="hero is-medium is-primary is-bold">
+      <div class="hero-body">
+        <div class="container">
+          <h1 v-t="'credits.title'" class="title" />
           <div v-for="(line, lineIndex) in $t('credits.mainText')" :key="lineIndex" v-html="line" class="container" />
         </div>
+      </div>
+    </section>
 
-        <div class="card credit-item">
-          <div class="card-content">
-            <div>Icons made by <a noreferer noopener target="_blank" href="https://www.flaticon.com/authors/freepik" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></div>
+    <section class="section has-background-white">
+      <div class="container">
+        <div class="content">
+          <div class="card credit-item">
+            <div class="card-content">
+              <div>Icons made by <a noreferer noopener target="_blank" href="https://www.flaticon.com/authors/freepik" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></div>
+            </div>
           </div>
-        </div>
 
-        <div class="card credit-item">
-          <div class="card-content">
-            UI : <a noreferer noopener target="_blank" href="https://bulma.io/">Bulma</a>
+          <div class="card credit-item">
+            <div class="card-content">
+              UI : <a noreferer noopener target="_blank" href="https://bulma.io/">Bulma</a>
+            </div>
           </div>
         </div>
       </div>
-    </div>
-  </section>
+    </section>
+  </div>
 </template>
 
 <style lang="scss" scoped>
@@ -28,9 +34,5 @@
     &:not(:last-child) {
       margin-bottom: 1em;
     }
-  }
-
-  .page-description {
-    margin-bottom: 1em;
   }
 </style>
