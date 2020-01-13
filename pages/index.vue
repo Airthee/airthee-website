18f27cc8022f1676e1322b23afbc2cbe69283531
Airthee <13355624+Airthee@users.noreply.github.com>
2020-01-13 16:53:16 +0100

    :construction: Add button
    problem : list is not updated when we click no "+" button

--- pages/index.vue
+++ pages/index.vue
@@ -14,22 +14,22 @@
       </div>
     </section>
 
+    <!-- General informations -->
     <section id="section-informations" class="main-section">
       <div class="container">
         <h3 :data-aos="randomAOSAnimation()" v-t="'home.sections.informations.title'" class="subtitle is-3" />
         <div :data-aos="randomAOSAnimation()" class="card">
           <div class="card-content">
-            <p class="content">
-              <ul>
-                <li v-for="(information, i) in $t('informations')" :key="i" v-html="information" />
-              </ul>
-            </p>
+            <div class="content">
+              <div v-for="(information, i) in $t('informations')" :key="i" v-html="information" />
+            </div>
           </div>
         </div>
       </div>
       <!-- <scroll-to target="#section-skills" /> -->
     </section>
 
+    <!-- History -->
     <section id="section-history" class="main-section">
       <div class="container">
         <h3 :data-aos="randomAOSAnimation()" v-t="'home.sections.history.title'" class="subtitle is-3" />
@@ -41,11 +41,12 @@
       </div>
     </section>
 
+    <!-- Skills -->
     <section id="section-skills" class="main-section">
       <div class="container">
         <h3 :data-aos="randomAOSAnimation()" v-t="'home.sections.skills.title'" class="subtitle is-3" />
         <div class="columns is-multiline">
-          <div v-for="(skill, skillIndex) in $t('skills')" :key="skillIndex" class="column is-one-third">
+          <div v-for="(skill, skillIndex) in displayedSkills" :key="skillIndex" class="column is-one-third">
             <div :data-aos="randomAOSAnimation()">
               <div class="card">
                 <header class="card-header">
@@ -60,9 +61,18 @@
             </div>
           </div>
         </div>
+        <div>
+          <button
+            v-if="displayedSkills.length < skills.length"
+            @click="displayMoreSkills(3)"
+          >
+            Voir plus
+          </button>
+        </div>
       </div>
     </section>
 
+    <!-- Social networks -->
     <section id="section-socials" class="main-section">
       <div class="container">
         <h3 :data-aos="randomAOSAnimation()" v-t="'home.sections.socials.title'" class="subtitle is-3" />
@@ -88,6 +98,26 @@ export default {
     // ScrollTo
     RandomCitation,
     Timeline
+  },
+
+  data () {
+    return {
+      skills: []
+    }
+  },
+
+  computed: {
+    displayedSkills () {
+      return this.skills.filter(s => s.display === true)
+    }
+  },
+
+  created () {
+    // Display 5 first skills
+    this.skills = this.$t('skills').map((s, index) => {
+      s.display = index < 6
+      return s
+    })
   },
 
   mounted () {
@@ -139,6 +169,20 @@ export default {
         type = Object.keys(animations)[Math.floor(Math.random() * Object.keys(animations).length)]
       }
       return animations[type][Math.floor(Math.random() * animations[type].length)]
+    },
+
+    /**
+     * Display X more skills
+     */
+    displayMoreSkills (number) {
+      let skillsAdded = 0
+      this.skills.forEach((s) => {
+        if (skillsAdded === number) { return false }
+        if (s.display === false) {
+          s.display = true
+          skillsAdded++
+        }
+      })
     }
   }
 }
