56ba671f6ca92671a00956815d00beac901dbc11
Airthee <13355624+Airthee@users.noreply.github.com>
2020-01-16 18:41:03 +0100

    :bug: Reload page when user change language

---
+++ plugins/i18n/main.js
@@ -0,0 +1,8 @@
+export default function ({ app }) {
+  // beforeLanguageSwitch called right before setting a new locale
+  app.i18n.beforeLanguageSwitch = (oldLocale, newLocale) => {
+    app.router.afterEach((to, from) => {
+      window.location.reload()
+    })
+  }
+}
