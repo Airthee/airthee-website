dc322da7e53212cad25b98d7c7dde112c3b90ba0
Airthee <13355624+Airthee@users.noreply.github.com>
2020-01-16 18:49:14 +0100

    :bug: Do not reload but change location to new route

--- plugins/i18n/main.js
+++ plugins/i18n/main.js
@@ -2,7 +2,7 @@ export default function ({ app }) {
   // beforeLanguageSwitch called right before setting a new locale
   app.i18n.beforeLanguageSwitch = (oldLocale, newLocale) => {
     app.router.afterEach((to, from) => {
-      window.location.reload()
+      window.location.href = to.fullPath
     })
   }
 }
