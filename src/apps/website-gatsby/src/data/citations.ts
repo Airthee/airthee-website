6a171759e514e3b3f80876692d59be23d9083781
Raphaël TISON <13355624+Airthee@users.noreply.github.com>
2024-10-05 20:01:06 +0000

    add hero section

---
+++ src/apps/website-gatsby/src/data/citations.ts
@@ -0,0 +1,28 @@
+import { CitationProps } from "../components/RotatingCitations";
+
+export const citations: CitationProps[] = [
+  {
+    message:
+      "Un objectif n’est pas toujours censé être atteint, il sert surtout de direction, de moteur, et d’accélérateur.",
+    author: "Bruce Lee",
+  },
+  {
+    message:
+      "La seule façon de faire du bon travail est d’aimer ce que vous faites. Si vous n’avez pas encore trouvé, continuez à chercher.",
+    author: "Steve Jobs",
+  },
+  {
+    message:
+      "Croire en soi est la première étape pour atteindre un objectif. Si vous pensez que vous allez échouer, vous le ferez probablement.",
+    author: "Will Smith",
+  },
+  {
+    message:
+      "Si vous voulez mener une vie heureuse, associez-la à un objectif, et non à des personnes ou à des objets.",
+    author: "Albert Einstein",
+  },
+  {
+    message: "L'échec est le fondement de la réussite.",
+    author: "Lao Tseu",
+  },
+];
