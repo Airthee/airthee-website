f3a7a5e1b773ce52d3db2bbe04304879f357704e
Airthee <13355624+Airthee@users.noreply.github.com>
2026-06-09 22:33:42 +0200

    init first section template

--- src/apps/website-gatsby/src/pages/index.tsx
+++ src/apps/website-gatsby/src/pages/index.tsx
@@ -2,7 +2,11 @@ import * as React from "react"
 
 export default function IndexPage() {
   return (
-    <main>Hello airthee</main>
+    <main>
+      <section className="bg-fuchsia-800 h-screen flex justify-center items-center flex-col">
+        <div className="h-[50rem] w-[50rem] bg-white rounded-full"></div>
+      </section>
+    </main>
   )
 }
 
